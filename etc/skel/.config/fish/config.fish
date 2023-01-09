843acdc04fc80e6ff60c8f423567ae2182ad1b06
TrueRed <64306333+RisingYT@users.noreply.github.com>
2023-01-09 16:34:06 +0800

    remove xfce cfgs

--- etc/skel/.config/fish/config.fish
+++ etc/skel/.config/fish/config.fish
@@ -1,12 +1,4 @@
-echo You might be wondering on why the terminal ran automatically.
-echo This is because we need to add the final touch.
-echo Now we shall update the packages and retrieve the repo databases from the internet.
-echo Do not worry. This is safe. If asked for a password then type it in. 
-sudo pacman -Syyu
-cd ~/.config/fish
-cp config.fish.default config.fish
-rm config.fish.default
-cd ~/.config/autostart
-rm Alacritty.desktop
-echo All done. It is neccesarry but optional for you to reboot.
-killall alacritty
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+motivate
+end
